f9a2a2aeeebadb309e7ef75d5041c6ad96ac4b7f
Alois Schloegl <alois.schloegl@ist.ac.at>
2016-05-18 00:20:11 +0200

    [b4c/python] deprecate swig interface, and use solution based on module extion

--- biosig4c++/python/demo2.py
+++ biosig4c++/python/demo2.py
@@ -17,15 +17,23 @@
 import biosig
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 
 ## read header 
 HDR=biosig.header("data/Newtest17-256.bdf")
-print HDR
+#print HDR
+## extracting header fields
+H=json.loads(HDR)
+H['CHANNEL'][1]['Filter']
 
 ## read and display data
 A=biosig.data("data/Newtest17-256.bdf")
+NS=np.size(A,1)
 
-plt.plot(A[:,3:7])
+fig = plt.figure()
+ax  = fig.add_subplot(111)
+h   = plt.plot(np.arange(np.size(A,0))/H['Samplingrate'],A[:,3:7]);
+ax.set_xlabel('time [s]')
 plt.show()
 
 
